6a2bd27ea5ca6cbbb4d23b9caec642130a281b2f
Edwin Croes <ed_croes@hotmail.com>
2022-12-06 06:50:34 +0100

    Day 6... too easy?

---
+++ AoC2022.Tests/Day06Tests.cs
@@ -0,0 +1,7 @@
+namespace AoC2022.Tests;
+
+[TestClass]
+public class Day06Tests : DayTestsWithTestDataBase<Day06.Day06>
+{
+    public Day06Tests() : base("1198", "3120", "7", "19") { }
+}
---
+++ AoC2022/Day06/Day06.cs
@@ -0,0 +1,35 @@
+namespace AoC2022.Day06;
+
+public class Day06 : IMDay
+{
+    public string FilePath { private get; init; } = "Day06\\input.txt";
+
+    public async Task<string> GetAnswerPart1()
+    {
+        var input = await GetInput();
+        return GetUniqueRangeIndex(input, 4).ToString();
+    }
+
+    public async Task<string> GetAnswerPart2()
+    {
+        var input = await GetInput();
+        return GetUniqueRangeIndex(input, 14).ToString();
+    }
+
+    private static int GetUniqueRangeIndex(string value, int numberOfUniqueCharacters)
+    {
+        for (var i = numberOfUniqueCharacters; i <= value.Length; i++)
+        {
+            var option = value[(i - numberOfUniqueCharacters)..i];
+            if (option.Distinct().Count() == numberOfUniqueCharacters)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private async Task<string> GetInput() =>
+        (await File.ReadAllTextAsync(FilePath)).Trim();
+}
