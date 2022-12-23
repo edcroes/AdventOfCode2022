681b6610c8388cd8d77662f725eaeed780c8620c
Edwin Croes <ed_croes@hotmail.com>
2022-12-23 14:09:15 +0100

    Day 21, forgot test value for part 2

--- AoC2022.Tests/Day21Tests.cs
+++ AoC2022.Tests/Day21Tests.cs
@@ -3,5 +3,5 @@ namespace AoC2022.Tests;
 [TestClass]
 public class Day21Tests : DayTestsWithTestDataBase<Day21.Day21>
 {
-    public Day21Tests() : base("256997859093114", "TODO", "152", "301") { }
+    public Day21Tests() : base("256997859093114", "3952288690726", "152", "301") { }
 }
